760f019da960c01b938b230e2290f70f439abdf2
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2026-03-07 06:46:18 +0800

    feat:added outputs to the cluster module

--- terraform/module/environment/main.tf
+++ terraform/module/environment/main.tf
@@ -5,10 +5,16 @@ module "network" {
   availability_zones = ["us-east-1a", "us-east-1b"]
   bastion_ingress    = var.bastion_ingress
 }
+
 module "database" {
   source          = "../database"
   name            = var.name
   vpc_name        = module.network.vpc_name
   security_groups = [module.network.database_security_group]
   subnets         = module.network.database_subnets
+}
+
+module "cluster" {
+  source = "../cluster"
+  name   = var.name
 }
