5127ce737312d47fa4125f7fea8c63a5936eef75
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2026-03-06 12:25:34 +0800

    remove the build file from github

--- terraform/module/cluster/load_balancer.tf
+++ terraform/module/cluster/load_balancer.tf
@@ -31,6 +31,8 @@ resource "aws_lb" "this" {
 }
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
 
   default_action {
     type = "fixed-response"
